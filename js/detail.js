ce08a70b45d8dece8f49925e05d679e6be6c498b
lukby2457 <kjh7442455@gmail.com>
2024-08-01 09:45:34 +0900

    parameter로 id 받아오는 내용 처리

--- js/detail.js
+++ js/detail.js
@@ -1,12 +1,17 @@
+// Parameter로 넘어오는 id값 처리
+let parameters = new URL(location.href).searchParams;
+let movieId = parameters.get('id');
+
+
 //영화 정보
-fetch("https://api.themoviedb.org/3/movie/{movie_id}?language=ko", options)
+fetch(`https://api.themoviedb.org/3/movie/${movieId}?language=ko`, options)
   .then((response) => response.json())
   .then((response) => console.log(response))
   .catch((err) => console.error(err));
 
 //출연진
 fetch(
-  "https://api.themoviedb.org/3/movie/{movie_id}/credits?language=ko",
+  `https://api.themoviedb.org/3/movie/${movieId}/credits?language=ko`,
   options
 )
   .then((response) => response.json())
